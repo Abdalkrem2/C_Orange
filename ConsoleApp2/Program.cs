ec905a2fdbf34c31013b18cf56bc837b28da15c7
Abdalkrem2 <abdalkremalshbol8@gmail.com>
2026-07-29 13:17:58 +0300

    task2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static double CalculateAverage(double n1, double n2, double n3)
+        {
+            return (n1 + n2 + n3) / 3;
+
+        }
+
+        static void StudentNames()
+        {
+            List<string> names=new List<string>();
+            names.Add("abd");
+            names.Add("ahmad");
+            names.Add("khaled");
+            names.Add("abd");
+            names.Add("belal");
+            foreach(string name in names) 
+            Console.WriteLine(name);
+
+            names.Remove("abd");
+            foreach (string name in names)
+                Console.WriteLine(name);
+        }
+
+        static void Task3()
+        {
+            Dictionary<int,string>map=new Dictionary<int, string>();
+            map[0] = "ahmad";
+            map[1] = "ahmad";
+            map[2] = "ahmad";
+            foreach(var name in map) {  Console.WriteLine(name); }
+        }
+
+        //linQ
+        static void Greater(int[] nums)
+        {
+            var res = nums.Where(n => n > 15);
+            
+           foreach(var n in res)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+        static void Even(int[] nums)
+        {
+            var res=nums.Where(n => n%2==0);
+            foreach(var n in res)
+            {
+                Console.WriteLine($"{n}");
+            }    
+
+        }
+        static void FillterName(string[] names)
+        {
+            var res = names.Where(n => n.StartsWith("A"));
+            foreach(var n in res)
+                Console.WriteLine(n);
+        }
+        static void GreaterThan10(int[] nums)
+        {
+            var res= nums.FirstOrDefault(n=>n>10);
+            Console.WriteLine(res);
+        }
+
+        //Task5
+
+
+        // 1. Method without parameters
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome!");
+        }
+
+        // 2. Method with parameters
+        static void Greet(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+
+        // 3. Method with default parameter
+        static void ShowAge(string name, int age = 18)
+        {
+            Console.WriteLine($"{name} is {age} years old.");
+        }
+
+        // 4. Method that returns the sum
+        static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // 5. Method with an array parameter
+        static void PrintArray(int[] numbers)
+        {
+            Console.WriteLine("Array elements:");
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 10, 15, 20, 25, 30 };
+            string[] names = { "Ahmad", "Ali", "Sara", "Mona", "Omar" };
+
+
+            GreaterThan10(numbers);
+        }
+    }
+}
---
+++ Task_loops/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_loops
+{
+    internal class Program
+    {
+        //loop
+
+        //1
+        static void Zigizag(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    for (int j = i; j >= 1; j--)
+                        Console.Write(j);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for(int j=1; j <=i; j++)
+                    Console.Write(j);
+                    Console.WriteLine();
+                }
+            }
+         
+        }
+        //2
+        static void MirrorNumber(int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                for(int s=n-i-1; s >1; s--)
+                {
+                    Console.Write(" ");
+                }
+                for(int k=i;k>=1; k--)
+                {
+                    Console.Write(k);
+                }
+                Console.WriteLine();
+            }
+
+        }
+        //3
+        static void CenteredNumberPyramid(int n) { 
+        for(int i = 1; i <= n; i++)
+            {
+                for(int s1 = n - i; s1 >= 1; s1--)
+                {
+                    Console.Write(' ');
+                }
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                for(int k = i-1; k >= 1; k--)
+                {
+                    Console.Write(k);
+                }
+                for (int s1 = n - i; s1 >= 1; s1--)
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //4
+        static void SkipEven(int n)
+        {
+           
+            for (int i=1; i <= n; i++)
+            {
+                int c = 1;
+               for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(c);
+                    c += 2;
+                }
+               Console.WriteLine();
+            }
+        }
+
+        //5
+        static void RowSum(int n) {
+            for (int i = 1; i <= n; ++i)
+            {
+                for (int j = 1; j <= i; ++j)
+                {
+                    Console.Write(Math.Pow(2,i-1));
+                }
+                Console.WriteLine() ;
+            }
+
+        }
+
+        //6
+        static void AlternatingDirection(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = i; j >= 1; j--)
+                        Console.Write(j);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int j = 1; j <= i; j++)
+                        Console.Write(j);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        //7
+        static void HollowPyramidNumbers(int n)
+        {
+
+        }      
+
+        //9
+        static void IncrementalBlocks(int n)
+        {
+            int  x = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(x++);
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //10
+        static void PatternwithGaps(int n) { 
+            for(int i = 1; i <n; i++)
+            {
+                Console.Write("1");
+                for(int j = 1; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("1");
+                
+            }
+            for(int i = 1; i <= n+1; i++)
+            {
+                Console.Write("1");
+            }
+        }
+        static void Main(string[] args)
+        {
+            PatternwithGaps(4);
+
+        }
+    }
+}
